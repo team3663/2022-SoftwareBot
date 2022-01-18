6f8f8a8f6c798649cf59d6950b6a5e644a6c28a4
Yvonne Guan <yvonneguan70@gmail.com>
2022-01-17 21:39:04 -0800

    Add method/UpdateOdometry

--- SoftwareBot/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ SoftwareBot/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -6,13 +6,18 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.sensors.PigeonIMU;
 import com.swervedrivespecialties.swervelib.Mk3SwerveModuleHelper;
+import com.swervedrivespecialties.swervelib.Mk4SwerveModuleHelper;
 import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
 import com.swervedrivespecialties.swervelib.SwerveModule;
+
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
@@ -24,7 +29,6 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
   public static final double MAX_VOLTAGE = 12.0;
   
-  // FIXME motor free speed RPM = 6380?
   public static final double MAX_VELOCITY_METERS_PER_SECOND = 6380.0 / 60.0 *
           SdsModuleConfigurations.MK3_STANDARD.getDriveReduction() *
           SdsModuleConfigurations.MK3_STANDARD.getWheelDiameter() * Math.PI;
@@ -43,6 +47,11 @@ public class DrivetrainSubsystem extends SubsystemBase {
           new Translation2d(-DRIVETRAIN_TRACKWIDTH_METERS / 2.0, -DRIVETRAIN_WHEELBASE_METERS / 2.0)
   );
 
+  private final SwerveDriveOdometry odometry = new SwerveDriveOdometry(kinematics, getGyroscopeRotation(), new Pose2d());
+
+  private double timeStamp = 0;
+  private Pose2d pose = new Pose2d();
+
   private final PigeonIMU pigeon = new PigeonIMU(DRIVETRAIN_PIGEON_ID);
         // counter-clockwise rotation increases angle
   //private final AHRS m_navx = new AHRS(SPI.Port.kMXP, (byte) 200); // Cannot resolve SPI
@@ -57,46 +66,45 @@ public class DrivetrainSubsystem extends SubsystemBase {
   public DrivetrainSubsystem() {
     ShuffleboardTab tab = Shuffleboard.getTab("Drivetrain");
 
-    // FIXME change to Mk4SwerveModuleHelper; possibly change GearRatio
-    frontLeftModule = Mk3SwerveModuleHelper.createFalcon500(
+    // FIXME GearRatio
+    frontLeftModule = Mk4SwerveModuleHelper.createFalcon500(
             tab.getLayout("Front Left Module", BuiltInLayouts.kList)
                     .withSize(2, 4)
                     .withPosition(0, 0),
-            // STANDARD or FAST depending on your gear configuration
-            Mk3SwerveModuleHelper.GearRatio.STANDARD,
+            Mk4SwerveModuleHelper.GearRatio.L4,
             FRONT_LEFT_MODULE_DRIVE_MOTOR,
             FRONT_LEFT_MODULE_STEER_MOTOR,
             FRONT_LEFT_MODULE_STEER_ENCODER,
             FRONT_LEFT_MODULE_STEER_OFFSET
     );
 
-    frontRightModule = Mk3SwerveModuleHelper.createFalcon500(
+    frontRightModule = Mk4SwerveModuleHelper.createFalcon500(
             tab.getLayout("Front Right Module", BuiltInLayouts.kList)
                     .withSize(2, 4)
                     .withPosition(2, 0),
-            Mk3SwerveModuleHelper.GearRatio.STANDARD,
+            Mk4SwerveModuleHelper.GearRatio.L4,
             FRONT_RIGHT_MODULE_DRIVE_MOTOR,
             FRONT_RIGHT_MODULE_STEER_MOTOR,
             FRONT_RIGHT_MODULE_STEER_ENCODER,
             FRONT_RIGHT_MODULE_STEER_OFFSET
     );
 
-    backLeftModule = Mk3SwerveModuleHelper.createFalcon500(
+    backLeftModule = Mk4SwerveModuleHelper.createFalcon500(
             tab.getLayout("Back Left Module", BuiltInLayouts.kList)
                     .withSize(2, 4)
                     .withPosition(4, 0),
-            Mk3SwerveModuleHelper.GearRatio.STANDARD,
+            Mk4SwerveModuleHelper.GearRatio.L4,
             BACK_LEFT_MODULE_DRIVE_MOTOR,
             BACK_LEFT_MODULE_STEER_MOTOR,
             BACK_LEFT_MODULE_STEER_ENCODER,
             BACK_LEFT_MODULE_STEER_OFFSET
     );
 
-    backRightModule = Mk3SwerveModuleHelper.createFalcon500(
+    backRightModule = Mk4SwerveModuleHelper.createFalcon500(
             tab.getLayout("Back Right Module", BuiltInLayouts.kList)
                     .withSize(2, 4)
                     .withPosition(6, 0),
-            Mk3SwerveModuleHelper.GearRatio.STANDARD,
+            Mk4SwerveModuleHelper.GearRatio.L4,
             BACK_RIGHT_MODULE_DRIVE_MOTOR,
             BACK_RIGHT_MODULE_STEER_MOTOR,
             BACK_RIGHT_MODULE_STEER_ENCODER,
@@ -105,13 +113,15 @@ public class DrivetrainSubsystem extends SubsystemBase {
   }
 
   public void zeroGyroscope() {
-    pigeon.setFusedHeading(0.0);
-    // m_navx.zeroYaw();
+          pigeon.setFusedHeading(0.0);
+          // m_navx.zeroYaw();
   }
 
   public Rotation2d getGyroscopeRotation() {
-    return Rotation2d.fromDegrees(pigeon.getFusedHeading());
-    // FIXME compass turns 180 when robot rotates by 90
+    return new Rotation2d(pigeon.getFusedHeading());
+    // FIXME compass turns 180 when robot rotates by 90 (may be fixed by gear ratio)
+
+    // changed from fromDegrees to fromRadians while writing updateOdometry, not sure if this would cause problems 
 
     /* 
    if (m_navx.isMagnetometerCalibrated()) {
@@ -125,7 +135,22 @@ public class DrivetrainSubsystem extends SubsystemBase {
     this.chassisSpeeds = chassisSpeeds;
   }
 
-  @Override
+  public void updateOdometry() {
+          Translation2d lastTranslation = pose.getTranslation();
+
+          double lastTime = timeStamp;
+          timeStamp = Timer.getFPGATimestamp();
+          double dt = timeStamp - lastTime;
+
+          double dx = chassisSpeeds.vxMetersPerSecond * dt;
+          double dy = chassisSpeeds.vyMetersPerSecond * dt;
+          Translation2d translationChange = new Translation2d(dx, dy);
+
+          Rotation2d currentAngle = getGyroscopeRotation();
+          pose = new Pose2d(lastTranslation.plus(translationChange), currentAngle);
+  }
+
+@Override
   public void periodic() {
     SwerveModuleState[] states = kinematics.toSwerveModuleStates(chassisSpeeds);
     SwerveDriveKinematics.desaturateWheelSpeeds(states, MAX_VELOCITY_METERS_PER_SECOND);
@@ -134,5 +159,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
     frontRightModule.set(states[1].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[1].angle.getRadians());
     backLeftModule.set(states[2].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[2].angle.getRadians());
     backRightModule.set(states[3].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[3].angle.getRadians());
+
+    updateOdometry();
   }
 }
