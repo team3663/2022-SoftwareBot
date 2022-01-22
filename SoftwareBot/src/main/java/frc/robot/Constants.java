194a74b0f490f1914e685425d7481c3a229c5d20
DylanChi308 <76456393+DylanChi308@users.noreply.github.com>
2022-01-22 10:47:40 -0800

    1/22/2022 10:47

--- SoftwareBot/src/main/java/frc/robot/Constants.java
+++ SoftwareBot/src/main/java/frc/robot/Constants.java
@@ -1,50 +1,40 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
 public final class Constants {
-    /**
-     * The left-to-right distance between the drivetrain wheels
-     *
-     * Should be measured from center to center.
-     */
-    public static final double DRIVETRAIN_TRACKWIDTH_METERS = 1.0; // FIXME Measure and set trackwidth
-    /**
-     * The front-to-back distance between the drivetrain wheels.
-     *
-     * Should be measured from center to center.
-     */
-    public static final double DRIVETRAIN_WHEELBASE_METERS = 1.0; // FIXME Measure and set wheelbase
+    // left to right wheel, center to center
+    public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.5461; // 21.5 inches
 
-    public static final int DRIVETRAIN_PIGEON_ID = 0; // FIXME Set Pigeon ID
+    // front to back wheel, center to center
+    public static final double DRIVETRAIN_WHEELBASE_METERS = 0.5461; // 21.5 inches
 
-    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 0; // FIXME Set front left module drive motor ID
-    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 0; // FIXME Set front left module steer motor ID
-    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 0; // FIXME Set front left steer encoder ID
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set front left steer offset
+    public static final int DRIVETRAIN_PIGEON_ID = 13;
 
-    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 0; // FIXME Set front right drive motor ID
-    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 0; // FIXME Set front right steer motor ID
-    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 0; // FIXME Set front right steer encoder ID
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set front right steer offset
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1;
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 5;
+    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 9;
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(302.52);
 
-    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 0; // FIXME Set back left drive motor ID
-    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 0; // FIXME Set back left steer motor ID
-    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 0; // FIXME Set back left steer encoder ID
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set back left steer offset
+    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2;
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 6; 
+    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 10;
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(23.38);
 
-    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 0; // FIXME Set back right drive motor ID
-    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 0; // FIXME Set back right steer motor ID
-    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 0; // FIXME Set back right steer encoder ID
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set back right steer offset
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 3; 
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 7; 
+    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 11;
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(96.33);
+
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 4;
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 8;
+    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 12;
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(310.96);
+    
+    public static final double TRANSLATION_P_CONSTANT = .013;
+    public static final double TRANSLATION_I_CONSTANT = 0;
+    public static final double TRANSLATION_D_CONSTANT = 0;
+
+    public static final double ROTATION_P_CONSTANT = .0293;
+    public static final double ROTATION_I_CONSTANT = 0;
+    public static final double ROTATION_D_CONSTANT = 0;
+
 }
---
+++ SoftwareBot/src/main/java/frc/robot/drivers/Pigeon.java
@@ -0,0 +1,76 @@
+package frc.robot.drivers;
+
+import edu.wpi.first.wpilibj.interfaces.Accelerometer;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+
+import java.nio.Buffer;
+
+import com.ctre.phoenix.ErrorCode;
+import com.ctre.phoenix.sensors.PigeonIMU;
+import com.ctre.phoenix.sensors.PigeonIMU.CalibrationMode;
+
+
+public class Pigeon implements Gyro, Accelerometer {
+    
+    private final PigeonIMU pigeon;
+    private Range range;
+    double[] tiltAngles = new double[3];
+
+    public Pigeon (int deviceNumber){
+        pigeon = new PigeonIMU(deviceNumber);
+    }
+
+    
+    @Override
+    public void setRange(Range range) {
+        this.range = range;
+    }
+    @Override
+    /**
+     *  The acceleration along the x axis in g-forces
+     */
+    public double getX() {
+        pigeon.getAccelerometerAngles(tiltAngles);
+        return Math.sin(tiltAngles[0]);
+    }
+    @Override
+    /**
+     * The acceleration along the y axis in g-forces
+     */
+    public double getY() {
+        pigeon.getAccelerometerAngles(tiltAngles);
+        return Math.sin(tiltAngles[1]);
+    }
+    @Override
+    /**
+     * The acceleratoin along the z axis in g-forces
+     */
+    public double getZ() {
+        pigeon.getAccelerometerAngles(tiltAngles);
+        return Math.sin(tiltAngles[2]);
+    }
+    @Override
+    public void calibrate() {
+        pigeon.enterCalibrationMode(CalibrationMode.BootTareGyroAccel);
+    }
+    @Override
+    public void reset() {
+        pigeon.setFusedHeading(0.0);
+    }
+    @Override
+    public double getAngle() {       
+        return pigeon.getFusedHeading();
+    }
+    @Override
+    public double getRate() {
+        // TODO
+        return 0; //TODO fix this
+    }
+
+
+    @Override
+    public void close() throws Exception {
+        // TODO 
+    }
+
+}
