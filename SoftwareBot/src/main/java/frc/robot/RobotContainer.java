d1fcbedf9d7ebae8ef99a996bc0b0b8fc45a7d8b
frankwyang <frank.yang@cedarpark.org>
2022-02-06 11:43:34 -0800

    add paths using PathWeaver 2022

--- SoftwareBot/src/main/java/frc/robot/RobotContainer.java
+++ SoftwareBot/src/main/java/frc/robot/RobotContainer.java
@@ -8,13 +8,13 @@ import edu.wpi.first.math.controller.RamseteController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
+// import edu.wpi.first.math.trajectory.TrajectoryConfig;
+// import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.Command;
+// import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RamseteCommand;
+// import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.Button;
 import frc.robot.commands.DefaultDriveCommand;
@@ -72,61 +72,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public SequentialCommandGroup getAutonomousCommand() {
-
-    // Create a voltage constraint to ensure we don't accelerate too fast
-    var autoVoltageConstraint =
-        new DifferentialDriveVoltageConstraint(
-            new SimpleMotorFeedforward(
-                Constants.ksVolts,
-                Constants.kvVoltSecondsPerMeter,
-                Constants.kaVoltSecondsSquaredPerMeter),
-            Constants.kDriveKinematics,
-            10);
-
-    // Create config for trajectory
-    TrajectoryConfig config =
-        new TrajectoryConfig(
-                Constants.kMaxSpeedMetersPerSecond,
-                Constants.kMaxAccelerationMetersPerSecondSquared)
-            // Add kinematics to ensure max speed is actually obeyed
-            .setKinematics(Constants.kDriveKinematics)
-            // Apply the voltage constraint
-            .addConstraint(autoVoltageConstraint);
-
-    // An example trajectory to follow.  All units in meters.
-    Trajectory exampleTrajectory =
-        TrajectoryGenerator.generateTrajectory(
-            // Start at the origin facing the +X direction
-            new Pose2d(0, 0, new Rotation2d(0)),
-            // Pass through these two interior waypoints, making an 's' curve path
-            List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-            // End 3 meters straight ahead of where we started, facing forward
-            new Pose2d(3, 0, new Rotation2d(0)),
-            // Pass config
-            config);
-
-    RamseteCommand ramseteCommand =
-        new RamseteCommand(
-            exampleTrajectory,
-            m_robotDrive::getPose,
-            new RamseteController(Constants.kRamseteB, Constants.kRamseteZeta),
-            new SimpleMotorFeedforward(
-                Constants.ksVolts,
-                Constants.kvVoltSecondsPerMeter,
-                Constants.kaVoltSecondsSquaredPerMeter),
-            Constants.kDriveKinematics,
-            m_robotDrive::getWheelSpeeds,
-            new PIDController(Constants.kPDriveVel, 0, 0),
-            new PIDController(Constants.kPDriveVel, 0, 0),
-            // RamseteCommand passes volts to the callback
-            m_robotDrive::tankDriveVolts,
-            m_robotDrive);
-
-    // Reset odometry to the starting pose of the trajectory.
-    m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
-
-    // Run path following command, then stop at the end.
-    return ramseteCommand.andThen(() -> m_robotDrive.tankDriveVolts(0, 0));
     return new SequentialCommandGroup(
       new InstantCommand(() -> m_drivetrainSubsystem.resetGyroAngle(Rotation2d.fromDegrees(0)), m_drivetrainSubsystem),
       new C_AutoDrive(m_drivetrainSubsystem, new Translation2d(2, 0), 1.0, Math.toRadians(180), 1.0)
--- SoftwareBot/src/main/java/frc/robot/paths/ExampleTrajectory.java
+++ SoftwareBot/src/main/java/frc/robot/paths/ExampleTrajectory.java
@@ -13,22 +13,43 @@ class ExampleTrajectory {
     public void generateTrajectory() {
   
       // 2018 cross scale auto waypoints.
-      var sideStart = new Pose2d(Units.feetToMeters(1.54), Units.feetToMeters(23.23),
-          Rotation2d.fromDegrees(-180));
-      var crossScale = new Pose2d(Units.feetToMeters(23.7), Units.feetToMeters(6.8),
-          Rotation2d.fromDegrees(-160));
-  
+      // var sideStart = new Pose2d(Units.feetToMeters(1.54), Units.feetToMeters(23.23),
+      //     Rotation2d.fromDegrees(-180));
+      // var crossScale = new Pose2d(Units.feetToMeters(23.7), Units.feetToMeters(6.8),
+      //     Rotation2d.fromDegrees(-160));
+      
+      // try it ourselves
+      var startPoint = new Pose2d(Units.feetToMeters(0.0), Units.feetToMeters(0.0), Rotation2d.fromDegrees(0));
+      var endPoint = new Pose2d(Units.feetToMeters(1.5), Units.feetToMeters(0.0), Rotation2d.fromDegrees(0));
+
+      // example code
+      // var interiorWaypoints = new ArrayList<Translation2d>();
+      // interiorWaypoints.add(new Translation2d(Units.feetToMeters(1.5), Units.feetToMeters(0)));
+      // interiorWaypoints.add(new Translation2d(Units.feetToMeters(21.04), Units.feetToMeters(18.23)));
+
+      // try it ourselves
       var interiorWaypoints = new ArrayList<Translation2d>();
-      interiorWaypoints.add(new Translation2d(Units.feetToMeters(14.54), Units.feetToMeters(23.23)));
-      interiorWaypoints.add(new Translation2d(Units.feetToMeters(21.04), Units.feetToMeters(18.23)));
-  
-      TrajectoryConfig config = new TrajectoryConfig(Units.feetToMeters(12), Units.feetToMeters(12));
+      interiorWaypoints.add(new Translation2d(Units.feetToMeters(1.0), Units.feetToMeters(0)));
+      // interiorWaypoints.add(new Translation2d(Units.feetToMeters(2.0), Units.feetToMeters(0.0)));
+
+      // TrajectoryConfig config = new TrajectoryConfig(Units.feetToMeters(12), Units.feetToMeters(12));
+      // config.setReversed(true);
+
+      // try it ourselves
+      TrajectoryConfig config = new TrajectoryConfig(Units.feetToMeters(2), Units.feetToMeters(2));
       config.setReversed(true);
   
+      // var trajectory = TrajectoryGenerator.generateTrajectory(
+      //     sideStart,
+      //     interiorWaypoints,
+      //     crossScale,
+      //     config);
+
+      // try it ourselves
       var trajectory = TrajectoryGenerator.generateTrajectory(
-          sideStart,
+          startPoint,
           interiorWaypoints,
-          crossScale,
+          endPoint,
           config);
     }
   }
