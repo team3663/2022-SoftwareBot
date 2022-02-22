a28f998c8caaae0b404879713dcc21eaf02eda2c
Yvonne Guan <yvonneguan70@gmail.com>
2022-02-21 20:08:58 -0800

    update default drive

--- SoftwareBot/src/main/java/frc/robot/commands/DefaultDriveCommand.java
+++ SoftwareBot/src/main/java/frc/robot/commands/DefaultDriveCommand.java
@@ -6,7 +6,6 @@ import frc.robot.subsystems.DrivetrainSubsystem;
 
 import java.util.function.DoubleSupplier;
 
-// FIXME add snap rotation (probably has to move part of the xbox controller input here from the robotcontainer)
 public class DefaultDriveCommand extends CommandBase {
     private final DrivetrainSubsystem drivetrainSubsystem;
 
@@ -28,7 +27,7 @@ public class DefaultDriveCommand extends CommandBase {
 
     @Override
     public void initialize() {
-            drivetrainSubsystem.resetPosition();
+        drivetrainSubsystem.resetPosition();
     }
 
     @Override
